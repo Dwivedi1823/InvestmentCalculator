c7811802ea41de239968440ac313b77cdc337ad1
unknown <pyushkumar999@gmail.com>
2024-06-11 22:25:12 +0530

    AngularProjects

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  // results = input();
+  // @Input({}) results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+
+  constructor(private invesstmentService: InvestmentService) {}
+
+  get results() {
+    return this.invesstmentService.resultData;
+  }
+}
